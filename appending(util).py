b5fef106eb5d70db03eb54259ad3e8c168fab38e
ngabo <ngaboizo2000@gmail.com>
2025-12-01 13:48:19 +0200

    append util methode in python

---
+++ appending(util).py
@@ -0,0 +1,12 @@
+num = [1,2,3]
+num.append(4)
+print(num)
+
+# utility function for append
+def append(list, item):
+  # this is how appending modifies the list in-place
+  list[-1:] = [item]
+  return list
+
+append(num, 400)
+print(num, 'number')
